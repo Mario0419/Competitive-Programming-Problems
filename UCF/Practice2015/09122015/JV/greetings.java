5e3716993f5e88f9c350177dc0406f703c193ea7
Mario <mario.massad@gmail.com>
2015-09-18 21:47:32 -0400

    added correct cow

--- UCF/Practice2015/09122015/JV/greetings.java
+++ UCF/Practice2015/09122015/JV/greetings.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-
 public class greetings{
+
 	greetings(){
 		Scanner input = new Scanner(System.in);
 
@@ -11,106 +11,64 @@ public class greetings{
 			int B = input.nextInt();
 			int E = input.nextInt();
 
-			HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-			int time = 1;
-			int pos = 0;
-			int direct = 0;
-			for(int i = 0; i < B; i++){
-				int t = input.nextInt();
-				String direction = input.next();
+			int[] b = new int[B];
+			int[] e = new int[E];
+			int sumB = 0;
+			int sumE = 0;
+			for(int i = 0; i < b.length; i++){
+				int temp = input.nextInt();
+				String d = input.next();
 
-				
-				if(direction.equals("R"))
-					direct = 1;
-				else
-					direct = -1;
-
-				for(int j = 0; j < t; j++){
-
-					pos += direct;
-					map.put(time, pos);
-					time++;
-				}
+				if(d.equals("L"))
+					temp *= -1;
+				b[i] = temp;
+				sumB += Math.abs(b[i]);
 			}
 
-			int lastPos = pos;
-			int count = 0;
-			pos = 0;
-			time = 1;
-			boolean areApart = false;
-
-			for(int i = 0; i < E; i++){
-				int t = input.nextInt();
-				String direction = input.next();
-
-				if(direction.equals("R"))
-					direct = 1;
-				else
-					direct = -1;
-
-				for(int j = 0; j < t; j++){
-
-					pos += direct;
-					if(map.containsKey(time)){
-						
-						if(map.get(time) == pos && areApart){
-							areApart = false;
-							count++;
-						}else if(map.get(time) == pos && !areApart){
-							areApart = false;
-						}else{
-							areApart = true;
-						}
-					}else if(pos == lastPos && areApart){
-						areApart = false;
-						count++;
-					}else if(pos == lastPos && !areApart){
-						
-					}else{
-						areApart = true;
-					}
-					time++;
-				
-
-				}
+			for(int i = 0; i < e.length; i++){
+				int temp = input.nextInt();
+				String d = input.next();
+				if(d.equals("L"))
+					temp *= -1;
+				e[i] = temp;
+				sumE += Math.abs(e[i]);
 			}
-			System.out.println(count);
+
+			int max = Math.max(sumB, sumE);
+			b = getTime(b, max);
+			e = getTime(e, max);
+
+			int ans = 0;
+
+			for(int i = 1; i < e.length && i < b.length; i++){
+				if(b[i] == e[i] && b[i-1] != e[i-1])
+					ans++;
+			}
+			System.out.println(ans);
 		}
+	}
+
+	int[] getTime(int[] array, int value){
+
+		int[] arr = new int[value + 1];
+		int index = 1;
+
+		for(int i = 0; i < array.length; i++){
+			int step = array[i] > 0 ? 1 : -1;
+			for(int j = 0; j < Math.abs(array[i]); j++, index++)
+				arr[index] = arr[index-1] + step;
+		}
+
+		while(index < arr.length){
+			arr[index] = arr[index-1];
+			index++;
+		}
+		return arr;
+
+
 	}
 
 	public static void main(String[] args){
 		new greetings();
 	}
 }
-
-
-/*
-
-
-2
-4 5
-3 L
-5 R
-1 L
-2 R
-4 R
-1 L
-3 L
-4 R
-2 L
-3 7
-1 R
-2 R
-3 R
-3 R
-2 R
-1 R
-1 R
-1 L
-0 R
-0 R
-
-
-
-
-*/
